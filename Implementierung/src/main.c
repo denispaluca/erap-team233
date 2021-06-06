fbc80244a2e883b6e40732ff20508213336a9378
Mert Çorumlu <mert.corumlu@tum.de>
2021-06-06 20:11:15 +0300

    cli implemented

--- Implementierung/src/main.c
+++ Implementierung/src/main.c
@@ -4,10 +4,14 @@
 #include "entropy.h"
 #include "io_operations.h"
 
-const char* optstring = "-abc";
+const char* optstring = ":+m:ta";
 
 /*
+	JUST A DRAFT
 
+	-m => run mode asm|simd|c
+	-t => calculate time that program takes
+	-a => accuracy
 */
 
 int main(int argc, char *argv[]) {
@@ -31,26 +35,26 @@ int main(int argc, char *argv[]) {
 	// of an option with character code 1.
 
 	int opt;
-	//char** input_files[argc];
 
 	// Fetching option arguments
 	while( (opt = getopt(argc, argv, optstring)) != -1 ) {
 
 		switch (opt)
 		{
+		case 'm' :
+		case 't' :
 		case 'a' :
-		case 'b' :
-		case 'c' :
 		break;
 
-		case 1 :
-			// Add optarg to input files
-			printf("%p\n", (void*) optarg);
-			break;
-
+		// ===========================================================
+		// TODO: implement functionality
+		// ===========================================================
 		
+		case ':' :
+			fprintf(stderr, "Missing argumant for option %c\n", optopt);
+
 		default:
-			fprintf(stderr, "ananı bacını\n");
+			fprintf(stderr, "USAGE HERE\n");
 			exit(EXIT_FAILURE);
 			break;
 		}
@@ -59,25 +63,18 @@ int main(int argc, char *argv[]) {
 
 	// Fetching arguments
 	if (optind >= argc) {
-		fprintf(stderr, "Need to specify at least one input file!");
+		fprintf(stderr, "Need to specify at least one input file!\n");
 		exit(EXIT_FAILURE);
 	}
 
-	
-	// if (argc <= 1)
-	// {
-	// 	printf("To give input as a file include file path as a first argument.\n");
-	// 	printf("Example usage: ./a.out input.txt.\n");
-	// 	exit(1);
-	// }
 
-	// for (int32_t i = 1; i < argc ; ++i)
-	// {
-	// 	float entropy = file_entropy_c(argv[i]);
-	// 	if(entropy != -1)
-	// 	{
-	// 		printf("Entropy of a given probabilty distribution in file %s is: %f.\n", argv[i], entropy);
-	// 	}
-	// }
-	return EXIT_FAILURE;
+	for (; optind < argc ; ++optind) {
+		float entropy = file_entropy_c(argv[optind]);
+		if(entropy != -1)
+		{
+			fprintf(stderr, "Entropy of a given probabilty distribution in file %s is: %f.\n", argv[optind], entropy);
+		}
+	}
+
+	return EXIT_SUCCESS;
 }

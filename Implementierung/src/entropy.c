7b342b4af2aa8b0bcf804b52d81e7dccc3a46f5b
denispaluca <denispaluca@gmail.com>
2021-06-08 10:02:34 +0200

    Add log2approx_deg2

--- Implementierung/src/entropy.c
+++ Implementierung/src/entropy.c
@@ -9,7 +9,7 @@ float scalar_entropy(size_t len,  float* data)
 	float entropy = 0;
 	for (size_t i = 0 ; i < len ; ++i)
 	{
-		entropy -= data[i] * log2f(data[i]);
+		entropy -= data[i] * log2approx_deg4(data[i]);
 	}
 	return entropy;
 }
@@ -46,5 +46,23 @@ float log2approx_deg2(float x){
     m = *(float *) &iz;
 
     y = a1*m*m + a2*m + (exponent - a3);
+    return y;
+}
+
+float log2approx_deg4(float x){
+    uint32_t ix, iz;
+    int exponent;
+    float m, m2, y, a1 = -0.081615808f, 
+	a2 = 0.64514236f, a3 = -2.1206751f, 
+	a4 = 4.0700908f, a5 = -2.5128546f;
+
+    ix = *(uint32_t *)&x;
+    exponent = ((int32_t) ix >> 23) - 127;
+    iz = (ix & 0x7fffff) + 0x3f800000;
+    m = *(float *) &iz;
+
+    m2 = m*m;
+    y = a1*m2 + a2 * m;
+    y = y*m2 + a3*m2 + a4*m + (a5 + exponent);
     return y;
 }

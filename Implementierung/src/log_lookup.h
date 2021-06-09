172de92fa87ecf14aacbf4918ec8f715cbed5e58
denispaluca <denispaluca@gmail.com>
2021-06-09 10:06:08 +0200

    Fix lookup simd signature

--- Implementierung/src/log_lookup.h
+++ Implementierung/src/log_lookup.h
@@ -8,5 +8,5 @@
 
 void init_table(unsigned n, float* table);
 float log2_lookup(unsigned n, float* table, float x);
-__mm128 log2_lookup_simd(unsigned n, float* table, __mm128 x);
+__m128 log2_lookup_simd(unsigned n, float* table, __m128 x);
 #endif //IMPLEMENTIERUNG_LOG_LOOKUP_H

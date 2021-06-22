078ba563c72e6fedbbb90aa984d423dbe484e78f
Mert Çorumlu <mert.corumlu@tum.de>
2021-06-22 23:58:44 +0300

    added graph generator

---
+++ Implementierung/tests/tests.c
@@ -0,0 +1,60 @@
+#include "../src/entropy.h"
+#include <locale.h>
+#pragma GCC diagnostic ignored "-Wformat-security"
+
+
+
+double time_calc(size_t it, size_t n, const float* data, float (* log2_func) (float), float (*entropy)(size_t, const float*, float (*) (float))) {
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    for (size_t i = 0; i < it; i++) {
+        entropy(n, data, log2_func);
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    double time_secs = (end.tv_sec - start.tv_sec) + 1e-9 * (end.tv_nsec - start.tv_nsec);
+    return time_secs / ((double) it);
+}
+
+void print_out(double t) {
+    char* str = malloc(50);
+    sprintf(str, "%.16g ", t);
+    
+    for (size_t i = 0; i < 50; i++)
+    {
+        if (str[i] == '.')
+        str[i] = ',';
+    }
+
+    printf(str);
+    free(str);
+}
+
+
+int main() {
+    size_t it = 90000;
+    size_t n = 4;
+    const size_t step = 4;
+
+
+    for (; n < 20; n += step)
+    {
+        float* arr = entropy_c_urandom_non_uniform(n);
+
+        print_out(time_calc(it, n, arr, log2approx_deg2, scalar_entropy));
+        print_out(time_calc(it, n, arr, log2approx_deg4, scalar_entropy));
+        print_out(time_calc(it, n, arr, log2approx_arctanh, scalar_entropy));
+        print_out(time_calc(it, n, arr, log2_lookup, scalar_entropy));
+        // print_out(time_calc(it, n, arr, log2_lookup_simd, simd_entropy));
+        // print_out(time_calc(it, n, arr, log2_glibc_simd, simd_entropy));
+
+        printf("\n");
+        free(arr);
+    }
+    
+
+
+}

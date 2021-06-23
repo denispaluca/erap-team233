07b9aa0c9874a6901c196a04c5a0fb88b9d7b1f9
Mert Çorumlu <mert.corumlu@tum.de>
2021-06-23 19:38:30 +0300

    added test cases

--- Implementierung/tests/testfiles/test_gen.py
+++ Implementierung/tests/testfiles/test_gen.py
@@ -5,16 +5,16 @@ import sys
 np.set_printoptions(threshold = sys.maxsize)
 
 
-f = open("data_10_000_non-uni", "w+")
+f = open("data_100_000_uni", "w+")
 
 # Random Generation
 
-a = np.random.random(10000)
-f.write(str(a / sum(a)).lstrip("[").rstrip("]"));
+# a = np.random.random(100)
+# f.write(str(a / sum(a)).lstrip("[").rstrip("]"));
 
 # Uniform Generation
-# n = 1000000
-# a = [1/n] * n
-# f.write(' '.join(map(str, a)))
+n = 100000
+a = [1/n] * n
+f.write(' '.join(map(str, a)))
 
 f.close()

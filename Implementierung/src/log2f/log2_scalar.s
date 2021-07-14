f7629588df98116fc7800acfb31e37818569d7a9
Mert Çorumlu <mert.corumlu@tum.de>
2021-07-14 17:10:15 +0300

    added comments to log2 functions in assembly

--- Implementierung/src/log2f/log2_scalar.s
+++ Implementierung/src/log2f/log2_scalar.s
@@ -14,7 +14,7 @@
 .equ MANTISSA_BITS, 23
 
 // Macro for reduce_float() inline function
-.macro REDUCE_FLOAT label, reduce = 1
+.macro REDUCE_FLOAT label, const = "deg2", reduce = 1
 
 	// Extract exponent from IEEE Floating Number
 	movd eax, xmm0
@@ -22,14 +22,14 @@
 
 	// // Special case for denormal floating numbers
 	cmp eax, 0
-	jnz .L\label
+	jnz .Lnormal_\label
 
-	mulss xmm0, [rip + artanh_consts]
+	mulss xmm0, [rip + \const\()_consts]
 	movd eax, xmm0
 	shr eax, MANTISSA_BITS
 	sub eax, MANTISSA_BITS
 
-	.L\label:
+	.Lnormal_\label:
 	sub eax, BIAS	
 
 	.if \reduce
@@ -51,11 +51,19 @@
 .align 16
 log2_deg2_scalar_asm:
 
-	REDUCE_FLOAT "normal_deg2"
+	// Inline function to extract mantissa
+	REDUCE_FLOAT "deg2", "deg2"
 
 	// Apply approximation
-	// log_2(val) = -0.344845 * z^2 + 2.024658 * z - 1.674873 + exponent
+	// log_2(z) = -0.344845 * z^2 + 2.024658 * z - 1.674873 + exponent
 
+	/*
+	xmm15 = deg2_co3 + exponent
+	xmm14 = deg2_co1 * z + deg2_co2
+	xmm0  = xmm14 * z + xmm15
+	*/
+
+	// cast integer exponent to float
 	cvtsi2ss xmm15, eax
 	addss xmm15, [rip + deg2_consts + 1*4]
 
@@ -63,6 +71,7 @@ log2_deg2_scalar_asm:
 	mulss xmm14, xmm0
 	addss xmm14, [rip + deg2_consts + 3*4]
 
+	// xmm0 = z * (deg2_co1 * z + deg2_co2) + deg2_co3 + exponent
 	mulss xmm0, xmm14
 	addss xmm0, xmm15
 	
@@ -73,20 +82,32 @@ log2_deg2_scalar_asm:
 .align 16
 log2_deg4_scalar_asm:
 
-	REDUCE_FLOAT "normal_deg4"
+	// Inline function to extract mantissa
+	REDUCE_FLOAT "deg4", "deg4"
 
 	// Apply approximation
-	// log_2(val) = -0.081615808 * z^4 + 0.64514MANTISSA_BITS6 * z^3 + 0.64514MANTISSA_BITS6 * z^2 + 4.0700908 * z + -2.5128546 + exponent
+	// log_2(z) = -0.081615808 * z^4 + 0.645146 * z^3 + 0.645146 * z^2 + 4.0700908 * z + -2.5128546 + exponent
 	
-	// y0
+	/*
+	xmm15 = deg4_co5 + exponent
+	xmm14 = z^2
+	xmm13 = deg4_co2 * z + deg4_co3
+	xmm12 = deg4_co1 * xmm14
+	xmm13 = xmm13 + xmm12
+	xmm11 = deg4_co4 * z + xmm15
+	xmm0  = xmm12 * xmm14 + xmm15
+	*/
+	
+	// cast integer exponent to float
 	cvtsi2ss xmm15, eax
 	addss xmm15, [rip + deg4_consts + 1*4]
 
-	// x2
+	// x^2
 	movss xmm14, xmm0
 	mulss xmm14, xmm14
 
-	// y
+	// ===================================
+	// xmm13 = deg4_co1 * z^2 + deg4_co2 * z + deg4_co3
 	movss xmm13, [rip + deg4_consts + 2*4]
 	mulss xmm13, xmm0
 	addss xmm13, [rip + deg4_consts + 3*4]
@@ -95,13 +116,16 @@ log2_deg4_scalar_asm:
 	mulss xmm12, xmm14
 
 	addss xmm13, xmm12
-	// y
+	// ===================================
 
-	// z
+	// ===================================
+	// xmm11 = deg4_co4 * z + deg4_co5 + exponent
 	movss xmm11, [rip + deg4_consts + 5*4]
 	mulss xmm11, xmm0
 	addss xmm11, xmm15
+	// ===================================
 
+	// xmm0 = z^2 * (deg4_co1 * z^2 + deg4_co2 * z + deg4_co3) + deg4_co4 * z + deg4_co5 + exponent
 	movss xmm0, xmm13
 	mulss xmm0, xmm14
 	addss xmm0, xmm11
@@ -112,48 +136,76 @@ log2_deg4_scalar_asm:
 .align 16
 log2_artanh_scalar_asm:
 
-	REDUCE_FLOAT "normal_artanh"
+	// Inline function to extract mantissa
+	REDUCE_FLOAT "artanh", "artanh"
 
-	// x = (z-1) / (z + 1)
-	//log_2(val) = 2 * (x + x^3/3 + x^5/5) / ln2
+	// Apply Approximation
+	// log_2(z) = 2 * (z + 1/3 * z^3 + 1/5 * z^5) / ln2
 
+	// q = (z-1) / (z + 1)
+	//log_2(z) = 2 * (q + q^3/3 + q^5/5) / ln2
+
+	/*
+	xmm15 = (z-1) / (z + 1) = q
+	xmm14 = q^2
+	xmm13 = 1.0
+	xmm0  = (1/5 * q^5 + 1/3 * q^3 + q) * 2/ln2 + exponent
+	*/
+
+	// =====================================
 	// xmm13 = 1.0
+	// xmm15 = (z-1) / (z + 1)
 	movss xmm13, [rip + artanh_consts + 1*4]
 
-	// q
 	movss xmm15, xmm0
 	subss xmm15, xmm13
 
 	movss xmm14, xmm0
 	addss xmm14, xmm13
 
-	// xmm15 = q
 	divss xmm15, xmm14
+	// =====================================
 
 	// xmm14 = q^2
 	movss xmm14, xmm15
 	mulss xmm14, xmm14
 
-	// y
+	// ====================================
+	// xmm0 = 1/3 + 1/5 * q^2
 	movss xmm0, xmm14
 	mulss xmm0, [rip + artanh_consts + 2*4]
 	addss xmm0, [rip + artanh_consts + 3*4]
+	// ====================================
+
+	// ====================================
+	// xmm0 = q^2 * (1/3 + 1/5 * q^2) + 1
 	mulss xmm0, xmm14
 	addss xmm0, xmm13
+	// ====================================
 
+	// ====================================
+	// xmm0 = q * 2/ln2 * (q^2 * (1/3 + 1/5 * q^2) + 1)
 	mulss xmm0, xmm15
 	mulss xmm0, [rip + artanh_consts + 4*4]
+	// ====================================
 
+	// cast exponent to float
 	cvtsi2ss xmm15, eax
-	addss xmm0, xmm15 
 
+	// add exponent to log_2(z) so log_2(val) = log_2(z) + exponent
+	addss xmm0, xmm15 
 	ret
 
 // float log2_lookup_scalar(float x)
 .align 16
 log2_lookup_scalar_asm:
 
-	REDUCE_FLOAT "normal_lookup", 0
+	// Inline function to extract mantissa
+	// Lookup function loads denormalize const from deg2
+
+	// Parameter 0 indicates that no reducing needed after extracting 
+	// the exponent as we will take first n bits of mantissa as index
+	REDUCE_FLOAT "lookup", reduce = 0
 
 	// Get first n bits of mantiss to look in table
 	movd ecx, xmm0
@@ -161,6 +213,8 @@ log2_lookup_scalar_asm:
 	shr ecx, (MANTISSA_BITS - LOG_LOOKUP_TABLE_SIZE)
 
 	// log_lookup_table[index] + exponent
+
+	// cast exponent to float
 	cvtsi2ss xmm0, eax
 	addss xmm0, [log_lookup_table + 4*ecx]
 
--- Implementierung/src/log2f/log2_simd.s
+++ Implementierung/src/log2f/log2_simd.s
@@ -53,11 +53,19 @@
 .align 16
 log2_deg2_simd_asm:
     
+    // Inline function to extract mantissa
     REDUCE_FLOAT_SIMD
 
     // Apply approximation
 	// log_2(val) = -0.344845 * z^2 + 2.024658 * z - 1.674873 + exponent
 
+    /*
+	xmm15 = deg2_co3 + exponent
+	xmm14 = deg2_co1 * z + deg2_co2
+	xmm0  = xmm14 * z + xmm15
+	*/
+
+	// cast integer exponent to float
     cvtdq2ps xmm15, xmm15
     addps xmm15, [rip + deg2_co3]
 
@@ -65,6 +73,7 @@ log2_deg2_simd_asm:
 	mulps xmm14, xmm0
 	addps xmm14, [rip + deg2_co2]
 
+	// xmm0 = z * (deg2_co1 * z + deg2_co2) + deg2_co3 + exponent
 	mulps xmm0, xmm14
 	addps xmm0, xmm15
 
@@ -74,20 +83,32 @@ log2_deg2_simd_asm:
 .align 16
 log2_deg4_simd_asm:
 
+	// Inline function to extract mantissa
     REDUCE_FLOAT_SIMD
 
     // Apply approximation
-	// log_2(val) = -0.081615808 * z^4 + 0.64514MANTISSA_BITS6 * z^3 + 0.64514MANTISSA_BITS6 * z^2 + 4.0700908 * z + -2.5128546 + exponent
+	// log_2(z) = -0.081615808 * z^4 + 0.645146 * z^3 + 0.645146 * z^2 + 4.0700908 * z + -2.5128546 + exponent
 	
-	// y0
+	/*
+	xmm15 = deg4_co5 + exponent
+	xmm14 = z^2
+	xmm13 = deg4_co2 * z + deg4_co3
+	xmm12 = deg4_co1 * xmm14
+	xmm13 = xmm13 + xmm12
+	xmm11 = deg4_co4 * z + xmm15
+	xmm0  = xmm12 * xmm14 + xmm15
+	*/
+
+    // cast integer exponent to float
     cvtdq2ps xmm15, xmm15
 	addps xmm15, [rip + deg4_co5]
 
-	// x2
+	// x^2
 	movaps xmm14, xmm0
 	mulps xmm14, xmm14
 
-	// y
+	// ===================================
+	// xmm13 = deg4_co1 * z^2 + deg4_co2 * z + deg4_co3
 	movaps xmm13, [rip + deg4_co2]
 	mulps xmm13, xmm0
 	addps xmm13, [rip + deg4_co3]
@@ -96,13 +117,16 @@ log2_deg4_simd_asm:
 	mulps xmm12, xmm14
 
 	addps xmm13, xmm12
-	// y
+	// ===================================
 
-	// z
+	// ===================================
+	// xmm11 = deg4_co4 * z + deg4_co5 + exponent
 	movaps xmm11, [rip + deg4_co4]
 	mulps xmm11, xmm0
 	addps xmm11, xmm15
+    // ===================================
 
+	// xmm0 = z^2 * (deg4_co1 * z^2 + deg4_co2 * z + deg4_co3) + deg4_co4 * z + deg4_co5 + exponent
 	movaps xmm0, xmm13
 	mulps xmm0, xmm14
 	addps xmm0, xmm11
@@ -113,29 +137,64 @@ log2_deg4_simd_asm:
 .align 16
 log2_artanh_simd_asm:
 
+    // Inline function to extract mantissa
     REDUCE_FLOAT_SIMD
 
+    // Apply Approximation
+	// log_2(z) = 2 * (z + 1/3 * z^3 + 1/5 * z^5) / ln2
+
+    // q = (z-1) / (z + 1)
+	//log_2(z) = 2 * (q + q^3/3 + q^5/5) / ln2
+
+	/*
+    xmm15 = exponent
+	xmm14 = (z-1) / (z + 1) = q
+	xmm13 = q^2
+	xmm12 = 1.0
+	xmm0  = (1/5 * q^5 + 1/3 * q^3 + q) * 2/ln2 + exponent
+	*/
+
+	// =====================================
+	// xmm12 = 1.0
+	// xmm14 = (z-1) / (z + 1)
+    movaps xmm12, [rip + one_packed]
+
     movaps xmm14, xmm0
-    subps xmm14, [rip + one_packed]
+    subps xmm14, xmm12
 
     movaps xmm13, xmm0
-    addps xmm13, [rip + one_packed]
+    addps xmm13, xmm12
 
     divps xmm14, xmm13
+    // =====================================
 
+    // xmm13 = q^2
     movaps xmm13, xmm14
     mulps xmm13, xmm13
 
+    // ====================================
+	// xmm0 = 1/3 + 1/5 * q^2
     movaps xmm0, xmm13
     mulps xmm0, [rip + one_fifth]
     addps xmm0, [rip + one_third]
-    mulps xmm0, xmm13
-    addps xmm0, [rip + one_packed]
+    // ====================================
 
+    // ====================================
+	// xmm0 = q^2 * (1/3 + 1/5 * q^2) + 1
+    mulps xmm0, xmm13
+    addps xmm0, xmm12
+    // ====================================
+
+    // ====================================
+	// xmm0 = q * 2/ln2 * (q^2 * (1/3 + 1/5 * q^2) + 1)
     mulps xmm0, xmm14
     mulps xmm0, [rip + ln2_inverse_2]
+    // ====================================
 
+    // cast exponent to float
     cvtdq2ps xmm15, xmm15
+
+    // add exponent to log_2(z) so log_2(val) = log_2(z) + exponent
     addps xmm0, xmm15
 
     ret
@@ -143,6 +202,11 @@ log2_artanh_simd_asm:
 .align 16
 log2_lookup_simd_asm:
 
+    // Inline function to extract mantissa
+	// Lookup function loads denormalize const from deg2
+
+	// Parameter 0 indicates that no reducing needed after extracting 
+	// the exponent as we will take first n bits of mantissa as index
     REDUCE_FLOAT_SIMD 0
 
     cvtdq2ps xmm15, xmm15
@@ -151,25 +215,39 @@ log2_lookup_simd_asm:
     pand xmm0, [rip + mantissa_mask]
     psrld xmm0, (MANTISSA_BITS - LOG_LOOKUP_TABLE_SIZE)
     
-    // r8 1:0
+    // xmm0 = n4 | n3 | n2 | n1
+
+    // r8 = n2 | n1
     movq r8, xmm0
+
+    // xmm0 = x | x | n4 | n3
     movhlps xmm0, xmm0
-    // r9 3:2
+
+    // r9 = n4 | n3
     movq r9, xmm0
+
+    // xmm14 = table[r9[31:0]]
+    // xmm13 = table[r9[63:32]]
     movss xmm14, [log_lookup_table + 4*r9d]
     shr r9, 32
     movss xmm13, [log_lookup_table + 4*r9d]
 
+    // xmm14 = x | x | table[n4] | table[n3]
     unpcklps xmm14, xmm13
 
+    // xmm0 = table[r9[31:0]]
+    // xmm13 = table[r9[63:32]]
     movss xmm0, [log_lookup_table + 4*r8d]
     shr r8, 32
     movss xmm13, [log_lookup_table + 4*r8d]
 
+    // xmm0 = x | x | table[n2] | table[n1]
     unpcklps xmm0, xmm13
     
+    // xmm0 = table[n4] | table[n3] | table[n2] | table[n1]
     movlhps xmm0, xmm14
 
+    // Adding exponent
     addps xmm0, xmm15
 
     ret
